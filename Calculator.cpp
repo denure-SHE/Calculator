32ac31d3857fc1b90bec963f94b1bcf65edd4ca9
angels-999 <47331096+angels-999@users.noreply.github.com>
2020-02-07 13:57:34 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,18 +3,19 @@ using namespace std
 
 void main()           //beginning of main function, execution starts here
 {
-int a,b,result,choice;       //declaration of variables
+int a,b,result;
+char choice;     //declaration of variables
 cout<<"enter the numbers:";
 cin>>a>>b;.                 //Reading input from the user
-cout<<"1.addition\n2.subtraction\n3.multiplication\n4.division\n5.modulus\nenter your choice:";
-cin>>choice; // reading choice (1,2,3,4 & 5 are valid options)
+cout<<"enter your choice(+,-,*,/,%):";
+cin>>choice; // reading choice 
 switch(choice)
 {
-case 1:cout<<"sum="<<a+b;break; //calculation based on choice
-case 2:cout<<"difference="<<a-b;break;
-case 3:cout<<"product="<<a*b;break;
-case 4:cout<<"quotient="<<a/b;break;
-case 5:cout<<"remainder="<<a%b;break;
+case '+':cout<<"sum="<<a+b;break; //calculation based on choice
+case '-':cout<<"difference="<<a-b;break;
+case '*':cout<<"product="<<a*b;break;
+case '/':cout<<"quotient="<<a/b;break;
+case '%':cout<<"remainder="<<a%b;break;
 default:cout<<"invalid option"; //when unknown operation is given as choice
 }
 }
